4f7441cb706ca7c7a2a451b89e1822dad84f7217
aungmoemyint404 <73078950+aungmoemyint404@users.noreply.github.com>
2021-03-22 16:41:35 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# Facebook Top3 Tools | Termux Onley
+
+
+apt update && apt upgrade
+
+apt install git -y
+
+apt install python -y
+
+apt install python2 -y
+
+git clone https://github.com/aungmoemyint404/Facebook-Tools2020
+
+ls
+
+bash install.sh
+
+run.......
---
+++ install.sh
@@ -0,0 +1,14 @@
+apt install ruby -y
+gem install lolcat
+apt install python -y
+apt install nano
+apt install python2 -y
+apt install git -y
+apt install openssh -y
+apt install wget -y
+apt install php -y
+pip2 install requests
+pip2 install mechanize
+apt install curl -y
+apt install libicu libgnutls -y
+echo 'END'
